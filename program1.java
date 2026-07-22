fcaafaedba164ea567a1313553f589b806dc64db
Gouri <gouri.nandhan.work@gmail.com>
2026-07-22 13:14:18 +0530

    lab Cycle 2 Task 1(22/07/2026)

---
+++ program1.java
@@ -0,0 +1,10 @@
+class Student {
+    public Student() {
+        System.out.println("Welcome to student class.");
+    }
+}
+public class program1 {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+    }
+}
