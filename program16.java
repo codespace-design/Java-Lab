c7eea68a1774a7124fa559825167450b255e97cc
Gouri <gouri.nandhan.work@gmail.com>
2026-09-01 10:30:45 +0530

    Lab Cycle 2 Program 16

---
+++ program16.java
@@ -0,0 +1,13 @@
+public class program16 {
+    static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println("Fibonacci Number = " + fibonacci(n));
+    }
+}
